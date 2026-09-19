115fcfc38d29037b92fccb55dbb8965f0d398591
Parth Sharma <parthsh.161106@gmail.com>
2026-09-19 16:19:30 +0530

    PythonBasics

--- code.py
+++ code.py
@@ -1,2 +1,10 @@
-print("Hello World!, I am parth")
-print("Parth Sharma")
+name = "Parth"
+age = 45
+cgpa = 7.8
+isPass = True
+
+print(name, age)
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(isPass))
